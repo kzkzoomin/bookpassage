0944087232ee86836eb45c0d06d72211f2a4f098
kzkzoomin <kzkzoomin@gmail.com>
2019-11-23 23:35:09 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h2>あなたのお気に入りの本の一節を紹介しましょう</h2>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
